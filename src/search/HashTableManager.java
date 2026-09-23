b6bf289e08ae2de29921fbe2987510f50fa38bff
MRM.Ilham <iyaad670ilham@gmail.com>
2026-09-23 22:30:51 +0530

    feat: add student ID search using hash table

--- src/search/HashTableManager.java
+++ src/search/HashTableManager.java
@@ -21,13 +21,36 @@ public class HashTableManager {
     }
 
     private int hashFunction(String id) {
-    int hash = 0;
-
-    for (char c : id.toCharArray()) {
-        hash += c;
+        int hash = 0;
+        for (char c : id.toCharArray()) {
+            hash += c;
+        }
+        return Math.abs(hash) % CAPACITY;
     }
 
-    return Math.abs(hash) % CAPACITY;
-}
+    public void insert(Student student) {
+        int index = hashFunction(student.getId());
+        HashNode newNode = new HashNode(student);
+        if (table[index] == null) {
+            table[index] = newNode;
+        } else {
+            HashNode temp = table[index];
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
 
+    public Student search(String id) {
+        int index = hashFunction(id);
+        HashNode temp = table[index];
+        while (temp != null) {
+            if (temp.student.getId().equalsIgnoreCase(id)) {
+                return temp.student;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
 }
